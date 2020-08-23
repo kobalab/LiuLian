13fd43526b13a9646cf8cc4370859495e5ab6cd2
Satoshi Kobayashi <koba@yk.rim.or.jp>
2020-08-23 23:34:24 +0800

    リバースプロキシ使用の場合に設定するオプション --mount に関する処理を追加

--- bin/liulian.js
+++ bin/liulian.js
@@ -7,17 +7,23 @@ const express = require('express');
 
 const argv = yargs
     .option('port',     { alias: 'p', default: 3571 })
+    .option('mount',    { alias: 'm'                })
     .argv;
 const port = argv['port'];
+const mount = argv['mount'];
 
 const path = require('path');
 const locale_dir = path.join(__dirname, '../locale');
 const locale = require('../lib/util/locale')(locale_dir, 'en');
 
-const liulian = require('../lib/liulian')(locale);
+const liulian = require('../lib/liulian');
 
 const app = express();
-app.use('/liulian', liulian);
+if (mount) app.enable('trust proxy');
+app.use(liulian({
+    locale: locale,
+    mount:  mount,
+}));
 
 app.listen(port, ()=>{
     console.log(`Server start on http://127.0.0.1:${port}/`);
