31fe212b5fc6fc20d1f2e7ffc47d5cfa2baba891
Satoshi Kobayashi <koba@yk.rim.or.jp>
2020-08-31 09:32:53 +0800

    起動オプション --base を削除

--- bin/liulian.js
+++ bin/liulian.js
@@ -10,13 +10,11 @@ const argv = yargs
     .usage('Usage: $0 <app-dir>')
     .option('port',     { alias: 'p', default: 3571 })
     .option('mount',    { alias: 'm'                })
-    .option('base',     { alias: 'b',               })
     .demandCommand(1)
     .argv;
 const home  = argv._[0];
 const port  = argv.port;
 const mount = argv.mount;
-const base  = (argv.base || '').replace(/\/$/,'');
 
 const path = require('path');
 
@@ -41,20 +39,20 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({extended: false}));
-app.post(`${base}/LOGIN`,
+app.post('/LOGIN',
     passport.authenticate('local', {
         successRedirect: './LOGIN?SUCESS',
         failureRedirect: './LOGIN?ERROR'
     })
 );
-app.use(`${base}/css`, express.static(path.join(__dirname, '../css')));
-app.use(base, liulian({
+app.use('/css', express.static(path.join(__dirname, '../css')));
+app.use(liulian({
     locale: locale,
     mount:  mount,
 }));
 
 app.listen(port, ()=>{
-    console.log(`Server start on http://127.0.0.1:${port}${base}`);
+    console.log(`Server start on http://127.0.0.1:${port}`);
 }).on('error', (e)=>{
     console.error(''+e);
     process.exit(-1);
