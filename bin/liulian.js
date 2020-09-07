2393986069fafcd08629596686f8b97639166cbf
Satoshi Kobayashi <koba@yk.rim.or.jp>
2020-09-07 13:27:12 +0800

    multer を use するタイミングを変更

--- bin/liulian.js
+++ bin/liulian.js
@@ -50,8 +50,8 @@ app.use(session);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({extended: false}));
-app.use(upload.fields([{name: 'file'}]));
 app.use('/css', express.static(path.join(__dirname, '../css')));
+app.use(upload.fields([{name: 'file'}]));
 app.use(liulian);
 
 app.listen(port, ()=>{
