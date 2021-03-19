22ab5608591646bde1f705416b0a8ee174b986f2
Satoshi Kobayashi <koba@yk.rim.or.jp>
2021-03-19 11:25:49 +0800

    起動時に作業ディレクトリを初期化する処理を追加

--- bin/liulian.js
+++ bin/liulian.js
@@ -15,6 +15,8 @@ const home  = argv._[0];
 const port  = argv.port;
 const mount = argv.mount;
 
+require('../lib/setup')(home);
+
 const locale   = require('../lib/util/locale')(
                             path.join(__dirname, '../locale'),
                             'en');
