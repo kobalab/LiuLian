ed3ab5b33c816e5a2a036bbb7311317b08b389c1
Satoshi Kobayashi <koba@yk.rim.or.jp>
2021-12-22 05:49:27 +0800

    #4 path → location に変更

--- test/backup.git.js
+++ test/backup.git.js
@@ -3,12 +3,14 @@ const fs     = require('fs');
 const path   = require('path');
 
 const GitDir = path.join(__dirname, 'GitDir')
+const location = '/dir/test.txt';
 
 suite('backup/git', ()=>{
 
     suiteSetup(()=>{
         fs.mkdirSync(GitDir);
         fs.mkdirSync(path.join(GitDir, '.git'));
+        fs.mkdirSync(path.join(GitDir, 'dir'));
     });
 
     test('モジュールが存在すること', ()=>assert.ok(require('../lib/backup/git')));
@@ -17,52 +19,52 @@ suite('backup/git', ()=>{
         let git = require('../lib/backup/git')(GitDir);
         assert.ok(git);
 
-        await git.log('test.txt');
+        await git.log(location);
     });
 
     test('checkIn()', async ()=>{
         const git = require('../lib/backup/git')(GitDir);
 
-        fs.writeFileSync(path.join(GitDir, 'test.txt'), 'revision 1\n');
+        fs.writeFileSync(path.join(GitDir, location), 'revision 1\n');
         let time1 = Math.floor(Date.now() / 1000) * 1000;
 
-        await git.checkIn('test.txt', 'user');
+        await git.checkIn(location, 'user');
 
         let time2 = Math.floor(Date.now() / 1000) * 1000;
-        let log = await git.log('test.txt');
+        let log = await git.log(location);
 
         assert.ok(time1 <= log[0].time && log[0].time <= time2);
 
-        await git.checkIn('test.txt', 'user');
+        await git.checkIn(location, 'user');
     });
 
     test('log()', async ()=>{
         const git = require('../lib/backup/git')(GitDir);
 
-        fs.writeFileSync(path.join(GitDir, 'test.txt'), 'revision 2\n');
+        fs.writeFileSync(path.join(GitDir, location), 'revision 2\n');
         let time1 = Math.floor(Date.now() / 1000) * 1000;
-        await git.checkIn('test.txt', 'user');
+        await git.checkIn(location, 'user');
         let time2 = Math.floor(Date.now() / 1000) * 1000;
-        fs.writeFileSync(path.join(GitDir, 'test.txt'), 'revision 3\n');
+        fs.writeFileSync(path.join(GitDir, location), 'revision 3\n');
 
-        let log = await git.log('test.txt');
+        let log = await git.log(location);
 
         assert.equal(log.length, 2);
     });
 
     test('diff()', async ()=>{
         const git = require('../lib/backup/git')(GitDir);
-        let log = await git.log('test.txt');
-        assert.deepEqual(await git.diff('test.txt', log[1].rev, log[0].rev),
+        let log = await git.log(location);
+        assert.deepEqual(await git.diff(location, log[1].rev, log[0].rev),
                         [ '@@ -1 +1 @@', '-revision 1', '+revision 2', '' ]);
-        assert.deepEqual(await git.diff('test.txt', log[1].rev),
+        assert.deepEqual(await git.diff(location, log[1].rev),
                         [ '@@ -1 +1 @@', '-revision 1', '+revision 3', '' ]);
     });
 
     test('checkOut()', async ()=>{
         const git = require('../lib/backup/git')(GitDir);
-        let log = await git.log('test.txt');
-        assert.equal(await git.checkOut('test.txt', log[1].rev),
+        let log = await git.log(location);
+        assert.equal(await git.checkOut(location, log[1].rev),
                                         'revision 1\n');
     });
 
